aa1d8c7f2cddd7ea5b3366607786e5afd3847dc5
Joshua Bevers <infernezor@gmail.com>
2021-05-28 16:03:00 -0400

    JB:(FEAT): Added Item to the CLI. Adds item based on price and name.

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -6,8 +6,11 @@ import org.apache.logging.log4j.Logger;
 import com.qa.ims.controller.Action;
 import com.qa.ims.controller.CrudController;
 import com.qa.ims.controller.CustomerController;
+import com.qa.ims.controller.ItemController;
 import com.qa.ims.persistence.dao.CustomerDAO;
 import com.qa.ims.persistence.domain.Domain;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.DBUtils;
 import com.qa.ims.utils.Utils;
 
@@ -16,11 +19,14 @@ public class IMS {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	private final CustomerController customers;
+	private final ItemController items;
 	private final Utils utils;
 
 	public IMS() {
 		this.utils = new Utils();
 		final CustomerDAO custDAO = new CustomerDAO();
+		final ItemDAO itemDAO = new ItemDAO();
+		this.items = new ItemController(itemDAO, utils);
 		this.customers = new CustomerController(custDAO, utils);
 	}
 
@@ -50,6 +56,7 @@ public class IMS {
 				active = this.customers;
 				break;
 			case ITEM:
+				active = this.items;
 				break;
 			case ORDER:
 				break;
@@ -75,16 +82,16 @@ public class IMS {
 	public void doAction(CrudController<?> crudController, Action action) {
 		switch (action) {
 		case CREATE:
-			crudController.create();
+			crudController.create(); // we need to modify code here.
 			break;
 		case READ:
-			crudController.readAll();
+			crudController.readAll(); //
 			break;
 		case UPDATE:
-			crudController.update();
+			crudController.update(); //
 			break;
 		case DELETE:
-			crudController.delete();
+			crudController.delete(); //
 			break;
 		case RETURN:
 			break;
---
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -0,0 +1,83 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import com.qa.ims.persistence.domain.Item;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.utils.Utils;
+
+/**
+ * Takes in Item details for CRUD functionality
+ *
+ */
+public class ItemController implements CrudController<Item> {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    private ItemDAO ItemDAO;
+    private Utils utils;
+
+    public ItemController(ItemDAO ItemDAO, Utils utils) {
+        super();
+        this.ItemDAO = ItemDAO;
+        this.utils = utils;
+    }
+
+    /**
+     * Reads all Items to the logger
+     */
+    @Override
+    public List<Item> readAll() {
+        List<Item> Items = ItemDAO.readAll();
+        for (Item Item : Items) {
+            LOGGER.info(Item);
+        }
+        return Items;
+    }
+
+    /**
+     * Creates a Item by taking in user input
+     */
+    @Override
+    public Item create() {
+        LOGGER.info("Please enter a item name");
+        String itemName = utils.getString();
+        LOGGER.info("Please enter a price");
+        Double itemPrice = utils.getDouble();
+        Item Item = ItemDAO.create(new Item(itemName, itemPrice));
+        LOGGER.info("Item created");
+        return Item;
+    }
+
+    /**
+     * Updates an existing Item by taking in user input
+     */
+    @Override
+    public Item update() {
+        LOGGER.info("Please enter the id of the Item you would like to update");
+        Long id = utils.getLong();
+        LOGGER.info("Please enter a item name");
+        String itemName = utils.getString();
+        LOGGER.info("Please enter the item's MSRP");
+        Double itemPrice = utils.getDouble();
+        Item Item = ItemDAO.update(new Item(id, itemName, itemPrice));
+        LOGGER.info("Customer Updated");
+        return Item;
+    }
+
+    /**
+     * Deletes an existing Item by the id of the Item
+     *
+     * @return
+     */
+    @Override
+    public int delete() {
+        LOGGER.info("Please enter the id of the Item you would like to delete");
+        Long id = utils.getLong();
+        return ItemDAO.delete(id);
+    }
+
+}
